da055112d441b8ab10c242769adc9f41b33b2904
shin <suoxin117@gmail.com>
2012-12-14 21:03:37 +0800

    init Pro

---
+++ server.js
@@ -0,0 +1,30 @@
+/*console.log("Hello World!--SuoXin");
+
+var http = require("http");
+
+http.createServer(function(request,response){
+	response.writeHead(200,{"Content-Type":"text/plain"});
+	response.write("Hello WWW");
+	response.end();
+}).listen(8888);
+*/
+
+var http 	= require("http");
+var url 	= require("url");
+
+function start(route,handle){
+	function onRequest(request, response) { 
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for" + pathname + "received."); 
+		
+		route(handle,pathname);
+		
+		response.writeHead(200, {"Content-Type": "text/plain"}); 
+		response.write("Hello World");
+	  	response.end();
+	}
+  
+	http.createServer(onRequest).listen(8888); console.log("Server has started.");
+}
+
+exports.start = start;
