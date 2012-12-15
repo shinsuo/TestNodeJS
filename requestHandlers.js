f6542edfa2f088caf7d452cfd2a2c9cdaa6e1511
shin <shin@ubuntu.(none)>
2012-12-15 05:02:36 -0800

    odify Logic

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,32 @@
+
+var exec = require("child_process").exec;
+
 function start(){
 	console.log("Request handler 'start' was called");
+	
+	/*
+	function sleep(milliSeconds){
+		var startTime = new Date().getTime();
+		while(new Date().getTime() < startTime + milliSeconds)
+		{
+			console.log(new Date().getTime());
+		}
+	};
+	
+	sleep(10000);
+	*/
+	
+	var content = "empty";
+	exec("ls -lah",function(error,stdout,stderr){
+		content = stdout;
+	});
+	
+	return "Hello Start\n" + content;
 }
 
 function upload(){
 	console.log("Request handler 'upload' was called");
+	return "Hello Upload";
 }
 
 exports.start = start;
--- router.js
+++ router.js
@@ -3,9 +3,10 @@ function route(handle,pathname){
 	
 	if (typeof handle[pathname] === 'function') {
 		
-		handle[pathname]();
+		return handle[pathname]();
 	}else {
 		console.log("No request handler found for "+pathname);
+		return "404 Not found!";
 	}
 }
 
